3faa7c3a5ec928bd8309368dc9a9d23b084fc134
Louis <huynguyennbk@gmail.com>
2022-09-25 10:11:21 +0700

    init project

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,15 @@
+package com.example
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.example.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureSerialization()
+        configureMonitoring()
+        configureHTTP()
+        configureSecurity()
+        configureRouting()
+    }.start(wait = true)
+}
